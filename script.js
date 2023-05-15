f5f42dbfd808d13f38e00827535b7162adb67112
Chris Cazaly <ccazaly95@gmail.com>
2023-05-15 21:11:56 +0100

    add complete/remove buttons

--- script.js
+++ script.js
@@ -4,13 +4,37 @@ const todolist = document.querySelector("#todolist");
 textarea.addEventListener("keydown", (e) => {
   // if key is enter
   if (e.key != "Enter") return;
+
   e.preventDefault();
+
   const textvalue = textarea.value;
   e.target.value = "";
-  console.log(textvalue);
 
-  const li = document.createElement("li");
-  li.innerText = textvalue;
-  console.log(li);
-  todolist.appendChild(li);
+  const div = document.createElement("div");
+  div.classList.add("tododiv");
+
+  const p = document.createElement("p");
+  p.classList.add("todop");
+  p.innerText = textvalue;
+
+  const removebutton = document.createElement("button");
+  removebutton.onclick = () => {
+    todolist.removeChild(div);
+  };
+  removebutton.innerText = "✖";
+  removebutton.classList.add("button");
+
+  const completebutton = document.createElement("button");
+  completebutton.onclick = () => {
+    p.classList.toggle("complete");
+  };
+  completebutton.innerText = "✓";
+  completebutton.classList.add("button");
+
+  div.appendChild(p);
+  div.appendChild(completebutton);
+  div.appendChild(removebutton);
+  todolist.appendChild(div);
+
+  console.log(div);
 });
